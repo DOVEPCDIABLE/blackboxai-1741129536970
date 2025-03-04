7e42bdcbdecc3219c4780d1703372cf5098ef53b
Abayomi Daniel <56458838+DOVEPCDIABLE@users.noreply.github.com>
2025-03-05 00:19:43 +0100

    Add src/init.php 1741130383163

---
+++ src/init.php
@@ -0,0 +1,10 @@
+<?php
+// Initialization file to include necessary models and controllers
+require_once __DIR__ . '/Models/User.php';
+require_once __DIR__ . '/Models/Wallet.php';
+require_once __DIR__ . '/Models/Transaction.php';
+require_once __DIR__ . '/Models/CryptoAsset.php';
+require_once __DIR__ . '/Models/GiftCard.php';
+require_once __DIR__ . '/Controllers/AuthController.php';
+require_once __DIR__ . '/Controllers/UserController.php';
+require_once __DIR__ . '/Controllers/TransactionController.php';
