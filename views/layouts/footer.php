5b898c6f81481962d79cfa7858b83501ed90dd53
Abayomi Daniel <56458838+DOVEPCDIABLE@users.noreply.github.com>
2025-03-05 00:05:56 +0100

    Add views/layouts/footer.php 1741129556355

---
+++ views/layouts/footer.php
@@ -0,0 +1,11 @@
+<?php
+// Footer Layout
+?>
+    </main>
+    <footer class="bg-gray-800 text-white p-4">
+        <div class="max-w-7xl mx-auto text-center">
+            <p>&copy; <?php echo date('Y'); ?> <?php echo APP_NAME; ?>. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
